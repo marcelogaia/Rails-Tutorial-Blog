51311ac06ab136cfd36a2d7194085478f2a3dbcf
Marcelo Gaia <marcelozakk07@gmail.com>
2016-09-03 20:26:02 -0700

    9.1 Basic Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "marcelo", password: "1234", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
